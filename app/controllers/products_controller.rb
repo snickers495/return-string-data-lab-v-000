943891b04745ec32996dabb7d917265a5cf97fd3
Lisa Lee <snickers495@gmail.com>
2018-03-08 22:09:32 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
   def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
   end
 
 
